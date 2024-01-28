bb75c761542b8b1c9a99191f5f1239d929690aef
Guilherme <gui.lima2304@gmail.com>
2024-01-28 18:23:40 -0300

    initial commit

---
+++ src/main/java/com/example/memeland/MemelandApplication.java
@@ -0,0 +1,13 @@
+package com.example.memeland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemelandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memeland/MemelandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memeland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
