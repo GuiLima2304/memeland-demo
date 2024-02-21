28a1a951d4138c29eab6e660e3e5b9175948909e
Guilherme <gui.lima2304@gmail.com>
2024-02-20 22:34:16 -0300

    Installed and configured DB integration and first controller

--- src/main/java/com/example/memeland/service/ClientService.java
+++ src/main/java/com/example/memeland/service/ClientService.java
@@ -1,4 +1,20 @@
 package com.example.memeland.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("client")
 public class ClientService {
+
+    @GetMapping("/")
+    public ResponseEntity<String> getAllClients() {
+        return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
+    }
+
+
 }
